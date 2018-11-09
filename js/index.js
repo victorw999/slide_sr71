71b807df21a053e263f4c7823f8a4c18f7e97ebc
victor linda_win7 <vicwong999@gmail.com>
2018-11-08 17:44:16 -0800

    First

---
+++ js/index.js
@@ -0,0 +1,86 @@
+var timelineSwiper = new Swiper ('.timeline .swiper-container', {
+  direction: 'vertical',
+  loop: false,
+  speed: 1600,
+  keyboard:{
+      enabled: true,
+  },
+
+  pagination: {
+      el: '.swiper-pagination',
+      type:'bullets',
+      dynamicBullets:true,
+      dynamicMainBullets:5,
+      clickable:true,
+      renderBullet: function (index, className) {
+          var year = document.querySelectorAll('.swiper-slide')[index].getAttribute('data-year');
+
+          //var inactives = document.querySelectorAll('span[class^=swiper-pagination-bullet ]');
+          //console.log(inactives);
+
+          return '<span class="' + className + '">'+ year + '</span>';
+      },
+
+  },
+  //paginationBulletRender:
+
+  //paginationClickable: true,
+  navigation:{
+    nextEl:'.swiper-button-next',
+    prevEl:'.swiper-button-prev',
+  },
+  //nextButton: '.swiper-button-next',
+  //prevButton: '.swiper-button-prev',
+  breakpoints: {
+    768: {
+      direction: 'horizontal',
+    }
+  },
+
+
+
+});
+
+// initial bullet formats
+var inactives = document.querySelectorAll(
+  //'.swiper-pagination span[class^=swiper-pagination-bullet]',
+  '.swiper-pagination span:not([class*=main])'
+  );
+//console.log(inactives);
+for (var i=0; i<inactives.length; i++){
+  inactives[i].classList.add('vicmod_inactive_bullet');
+  console.log(inactives[i]);
+}
+
+
+
+timelineSwiper.on('slideChange', function(swiper, paginationEl){
+  var all_bullets = document.querySelectorAll(
+    '.swiper-pagination span[class^=swiper-pagination-bullet]',
+  );
+  for (let i=0; i<all_bullets.length; i++){
+    if(all_bullets[i].classList.contains('vicmod_inactive_bullet')){
+      all_bullets[i].classList.remove('vicmod_inactive_bullet');
+      console.log(all_bullets[i] + ' REMOVED');
+    }
+  }
+
+
+  var inactives = document.querySelectorAll(
+    //'.swiper-pagination span[class^=swiper-pagination-bullet]',
+    '.swiper-pagination span:not([class*=main])'
+    );
+  //console.log(inactives);
+  for (let i=0; i<inactives.length; i++){
+    if(!inactives[i].classList.contains('vicmod_inactive_bullet')){
+      inactives[i].classList.add('vicmod_inactive_bullet');
+      console.log(inactives[i] + 'ADDED');
+    }else{
+      console.log('this one has already');
+    }
+
+  }
+
+
+
+});
